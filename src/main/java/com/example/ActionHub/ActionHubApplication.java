4e9d859f1e173f44ef3514d7e762b2358ee68b0f
priyanka <juyal.priyanka94@gmail.com>
2022-06-29 15:40:55 +0530

    Committing base package

---
+++ src/main/java/com/example/ActionHub/ActionHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.ActionHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ActionHub/ActionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ActionHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
